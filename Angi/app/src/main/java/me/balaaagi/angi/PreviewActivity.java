f970eef0abbe1e7384b78cbe205aa7737e67d015
Balaji Srinivasan <SBALAJ33@ford.com>
2017-09-17 03:34:21 +0530

    Added Camera Activities

---
+++ Angi/app/src/main/java/me/balaaagi/angi/PreviewActivity.java
@@ -0,0 +1,23 @@
+package me.balaaagi.angi;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class PreviewActivity extends AppCompatActivity {
+    ImageView previewImage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview);
+        previewImage= (ImageView) findViewById(R.id.capturedImagePreview);
+        Bitmap bitmap = ResultHolder.getImage();
+        if (bitmap == null) {
+            finish();
+            return;
+        }
+
+        previewImage.setImageBitmap(bitmap);
+    }
+}
